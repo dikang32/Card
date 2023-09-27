78172764270c43f6cae23a3c95daa5ad56d133bd
Khangnvd <khangnguyen.030204@gmail.com>
2023-09-27 22:17:30 +0700

    filter profession

--- src/App.jsx
+++ src/App.jsx
@@ -30,31 +30,33 @@ function App() {
             <List
               sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
             >
-              {PEOPLE.map((person) => (
-                <>
-                  <ListItem alignItems="flex-start">
-                    <ListItemAvatar>
-                      <Avatar alt="Remy Sharp" src={getImageUrl(person)} />
-                    </ListItemAvatar>
-                    <ListItemText
-                      primary={person.name}
-                      secondary={
-                        <>
-                          <Typography
-                            sx={{ display: "inline" }}
-                            component="span"
-                            variant="body2"
-                            color="text.primary"
-                          >
-                            {person.profession + " known for "}
-                          </Typography>
-                          {person.accomplishment}
-                        </>
-                      }
-                    />
-                  </ListItem>
-                </>
-              ))}
+              {PEOPLE.map((person) =>
+                person.profession === "chemist" ? (
+                  <>
+                    <ListItem alignItems="flex-start">
+                      <ListItemAvatar>
+                        <Avatar alt="Remy Sharp" src={getImageUrl(person)} />
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary={person.name}
+                        secondary={
+                          <>
+                            <Typography
+                              sx={{ display: "inline" }}
+                              component="span"
+                              variant="body2"
+                              color="text.primary"
+                            >
+                              {person.profession + " known for "}
+                            </Typography>
+                            {person.accomplishment}
+                          </>
+                        }
+                      />
+                    </ListItem>
+                  </>
+                ) : null
+              )}
             </List>
           </CardContent>
         </Card>
